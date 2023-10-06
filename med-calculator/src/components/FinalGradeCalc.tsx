5061c91ff9fb0a95c4a2e01bf219df4e730981c8
YoderBy <yosefbyd@gmail.com>
2023-10-06 14:46:58 +0300

    D

--- med-calculator/src/components/FinalGradeCalc.tsx
+++ med-calculator/src/components/FinalGradeCalc.tsx
@@ -23,14 +23,14 @@ interface RequiredGrades {
 
 
 
-export const getRequiredGrade = (university: string, year : number)=> {
+export const getRequiredGrade = (university: string, year: number) => {
   const key = year.toString();
   // Ensuring that "key" is used as a string index
   switch (university) {
     case 'tel aviv':
       return RequiredGrades.TelAvivUnivercityScore[key];
-//{Grades[key as keyof Grades]}
-      case 'heb':
+    //{Grades[key as keyof Grades]}
+    case 'heb':
       return RequiredGrades.HebrewUnivercityScore[key];
     case 'tech':
       return RequiredGrades.TechnionScore[key];
@@ -43,34 +43,46 @@ export const getRequiredGrade = (university: string, year : number)=> {
 export const RequiredGrades: RequiredGrades = {
   HebrewUnivercityScore: {
     2023: { WithMorScore: 26.183, WithoutMoreScore: 25.350 },
-    2022: { WithMorScore: 21.183, WithoutMoreScore: 24.350 },
-    2021: { WithMorScore: 23.183, WithoutMoreScore: 26.350 }
+    2022: { WithMorScore: 26.183, WithoutMoreScore: 24.350 },
+    2021: {
+      WithMorScore: 26.176
+      , WithoutMoreScore: 26.350
+    }
   },
   TelAvivUnivercityScore: {
     2023: { WithMorScore: 744.54, WithoutMoreScore: 726.58 },
-    2022: { WithMorScore: 724.54, WithoutMoreScore: 721.58 },
-    2021: { WithMorScore: 743.54, WithoutMoreScore: 734.58 }
+    2022: {
+      WithMorScore: 746.09
+      , WithoutMoreScore: 729.58
+    },
+    2021: {
+      WithMorScore: 746.41
+      , WithoutMoreScore: 734.58
+    }
   },
   TechnionScore: {
-    2023: { WithMorScore: 202, WithoutMoreScore: 91.925 },
-    2022: { WithMorScore: 204, WithoutMoreScore: 93.925 },
-    2021: { WithMorScore: 206, WithoutMoreScore: 97.925 }
+    2023: { WithMorScore: 201, WithoutMoreScore: 91.925 },
+    2022: {
+      WithMorScore: 203
+      , WithoutMoreScore: 93.925
+    },
+    2021: { WithMorScore: 207, WithoutMoreScore: 97.925 }
   }
 };
 export const RequiredGrades1 = {
-  HebrewUnivercityScore: 
-{ WithMorScore: 26.183, WithoutMoreScore: 25.350 },
+  HebrewUnivercityScore:
+    { WithMorScore: 26.183, WithoutMoreScore: 25.350 },
 
-  
+
   TelAvivUnivercityScore:
     { WithMorScore: 744.54, WithoutMoreScore: 726.58 },
-    
-  
+
+
   TechnionScore: {
-     WithMorScore: 202, WithoutMoreScore: 91.925 
-    
+    WithMorScore: 202, WithoutMoreScore: 91.925
+
   }
-} 
+}
 
 interface Props {
   Grades: Grades;
@@ -78,44 +90,33 @@ interface Props {
 }
 
 const FinalGradeCalc: React.FC<Props> = ({ Grades, setGrades }) => {
-  
+
   const { colorMode } = useColorMode();
   const isDarkMode = colorMode === "dark";
 
 
 
-const sortMin = (string: string) => {
-  switch(string){
-    case 'Bagrut': return 80
-    case 'Psych': return 200
-    case 'MorScore': return 150
+  const sortMin = (string: string) => {
+    switch (string) {
+      case 'Bagrut': return 80
+      case 'Psych': return 200
+      case 'MorScore': return 150
+    }
+    return 0;
   }
-  return 0;
-}
 
-const sortMax = (string: string) => {
-  switch(string){
-    case 'Bagrut': return 140
-    case 'Psych': return 800
-    case 'MorScore': return 250
+  const sortMax = (string: string) => {
+    switch (string) {
+      case 'Bagrut': return 140
+      case 'Psych': return 800
+      case 'MorScore': return 250
+    }
+    return 0;
   }
-  return 0;
-}
   const FinalGrades: FinalScoreWithAndWithoutMor = CalculateFinalGrade(Grades);
   const [yearToShow, setyearToShow] = useState(2023);
-  const RequiredFinalGrade =  getRequiredGrade(Grades.Univercity, yearToShow);
+  const RequiredFinalGrade = getRequiredGrade(Grades.Univercity, yearToShow);
 
-  // // const onSelect = (year:number)=>{
-  // //   switch(year){
-  // //     case 2023:
-  // //       setRequiredFinalGrade(getRequiredGrade(Grades.Univercity))
-  // //     case 2022:
-  // //       setRequiredFinalGrade(getRequiredGrade(Grades.Univercity))
-  // //     case 2021:
-  // //       setRequiredFinalGrade(getRequiredGrade(Grades.Univercity))
-          
-  // //   }
-  // }
   const handleInputChange = (value: string, key: string) => {
     const newGrades = { ...Grades, [key]: parseFloat(value) };
     setGrades(newGrades);
@@ -124,19 +125,19 @@ const sortMax = (string: string) => {
   return (
     <Flex direction="column" align="center" justify="center" p={4}>
       <HStack dir="rtl">
-      <Text dir="rtl">
-        בחר שנה:
-      </Text>
-           <Select 
-             w = '150px'
+        <Text dir="rtl">
+          בחר שנה:
+        </Text>
+        <Select
+          w='150px'
 
-            padding={'10px'}
-            dir='rtl' 
-            onChange={(e) =>setyearToShow(parseInt(e.target.value))}
-            defaultValue="סכם בשנת:">
-                <option  value="2023">2023</option>
-                <option value="2022">2022</option>
-                <option value="2021">2021</option>
+          padding={'10px'}
+          dir='rtl'
+          onChange={(e) => setyearToShow(parseInt(e.target.value))}
+          defaultValue="סכם בשנת:">
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
         </Select></HStack>
       <VStack
         w={{ base: '90%', md: '500px' }}
@@ -167,14 +168,14 @@ const sortMax = (string: string) => {
                 bg={isDarkMode ? '#444' : '#e6f7ff'}
                 color={isDarkMode ? 'white' : '#003366'}
               >
-                <NumberInputField textAlign = 'center' padding={'1px'}/>
-                </NumberInput>
-                <Text textAlign = 'right' fontSize="lg" color={isDarkMode ? 'white' : '#003366'}>:{label}</Text>
-              </HStack>
+                <NumberInputField textAlign='center' padding={'1px'} />
+              </NumberInput>
+              <Text textAlign='right' fontSize="lg" color={isDarkMode ? 'white' : '#003366'}>:{label}</Text>
+            </HStack>
           );
         })}
       </VStack>
- 
+
       <Box
         w={{ base: '90%', md: '500px' }}
         mt={5}
@@ -185,12 +186,12 @@ const sortMax = (string: string) => {
       >
         <VStack>
           <HStack align-items='inherit' >
-            <Text dir = 'rtl' fontSize={{ base: 'sm', md: 'md' }}
+            <Text dir='rtl' fontSize={{ base: 'sm', md: 'md' }}
               color={FinalGrades.WithoutMoreScore > RequiredFinalGrade.WithoutMoreScore ? "#28a745" : "#dc3545"}
             >
               סכם ראשוני ל{UnivercityNameChanger(Grades.Univercity)}: {RequiredFinalGrade.WithoutMoreScore}
             </Text>
-            <Text dir = 'rtl' fontSize={{ base: 'sm', md: 'md' }}
+            <Text dir='rtl' fontSize={{ base: 'sm', md: 'md' }}
               color={FinalGrades.WithMorScore > RequiredFinalGrade.WithMorScore ? "#28a745" : "#dc3545"}
             >
               סכם קבלה ל{UnivercityNameChanger(Grades.Univercity)}: {RequiredFinalGrade.WithMorScore}
@@ -208,19 +209,19 @@ const sortMax = (string: string) => {
 
           </VStack>
 
-            <VStack display={FinalGrades.WithoutMoreScore < RequiredFinalGrade.WithoutMoreScore ? '' : 'none'}
-            >
-              <Text dir = 'rtl' fontSize={{ base: 'sm', md: 'md' }}> על מנת לעבור סכם ראשוני יש לשפר אחד מהציונים:</Text>
-              <Text fontSize={{ base: 'sm', md: 'md' }}> פסיכומטרי : {calculateRequiredGradesToPass(Grades, yearToShow).requiredPsych}</Text>
-              <Text fontSize={{ base: 'sm', md: 'md' }}>ממוצע בגרות : {calculateRequiredGradesToPass(Grades, yearToShow).requiredBagrut}</Text>
-            </VStack>
+          <VStack display={FinalGrades.WithoutMoreScore < RequiredFinalGrade.WithoutMoreScore ? '' : 'none'}
+          >
+            <Text dir='rtl' fontSize={{ base: 'sm', md: 'md' }}> על מנת לעבור סכם ראשוני יש לשפר אחד מהציונים:</Text>
+            <Text fontSize={{ base: 'sm', md: 'md' }}> פסיכומטרי : {calculateRequiredGradesToPass(Grades, yearToShow).requiredPsych}</Text>
+            <Text fontSize={{ base: 'sm', md: 'md' }}>ממוצע בגרות : {calculateRequiredGradesToPass(Grades, yearToShow).requiredBagrut}</Text>
+          </VStack>
 
-            <VStack display={FinalGrades.WithMorScore < RequiredFinalGrade.WithMorScore ? '' : 'none'}>
-            <Text dir = 'rtl' fontSize={{ base: 'sm', md: 'md' }}> על מנת לעבור סכם קבלה יש לשפר אחד מהציונים:</Text>
-              
-              <Text fontSize={{ base: 'sm', md: 'md' }}> סכם ראשוני : {roundTwoDigits(calculateRequiredGradesToPass(Grades, yearToShow).requiredFinal)}</Text>
-              <Text fontSize={{ base: 'sm', md: 'md' }}>מור : {Math.floor(calculateRequiredGradesToPass(Grades, yearToShow).requiredMor)}</Text>
-            </VStack>
+          <VStack display={FinalGrades.WithMorScore < RequiredFinalGrade.WithMorScore ? '' : 'none'}>
+            <Text dir='rtl' fontSize={{ base: 'sm', md: 'md' }}> על מנת לעבור סכם קבלה יש לשפר אחד מהציונים:</Text>
+
+            <Text fontSize={{ base: 'sm', md: 'md' }}> סכם ראשוני : {roundTwoDigits(calculateRequiredGradesToPass(Grades, yearToShow).requiredFinal)}</Text>
+            <Text fontSize={{ base: 'sm', md: 'md' }}>מור : {Math.floor(calculateRequiredGradesToPass(Grades, yearToShow).requiredMor)}</Text>
+          </VStack>
         </VStack>
 
       </Box>
